42fb40b826aac75d542cf92f2959ef0daab1093f
Francisco Alex <franciscoalex.bio@gmail.com>
2021-10-20 20:10:59 -0300

    enviando a tabela dos jogos por email (formato csv)

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-from select import select
+# importações
 from selenium import webdriver
 import os 
-import time
-from selenium.webdriver import chrome
 from selenium.webdriver.support.ui  import Select
-from selenium.webdriver.chrome.options import Options
-import pandas
+import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-# WEBSCRAPING
-# ----------- 
+# FAZENDO O WEBSCRAPING
 
+# listas e dicionário
 deletar = []
 aux = []
 dados = {'times': [],
          'horario': []}
 
+# conectando o navegador
 navegador = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'))
 
+# link de acesso 
 url = navegador.get('https://www.cbf.com.br/')
 
+# navegando pelo site
 jogosHoje = navegador.find_element_by_xpath('//*[@id="app"]/header/div/nav/ul/li[3]/a/b').click()
-
 jogosHojeTdos = navegador.find_element_by_xpath('//*[@id="app"]/header/div/nav/ul/li[3]/a').click()
 
 # selecionando o campionanto dentro de um select 
 jogosHojeSerieA = navegador.find_element_by_id('competition-filter')
 selectElement = Select(jogosHojeSerieA)
 
-
 selectElement.select_by_index(0)
 jogos = navegador.find_elements_by_class_name('clearfix')
 
+# adicionando os jogos com o horário do jogo na lista auxiliar
 for i in jogos:
     jogo = i.text
     deletar.append(str(jogo).replace('\n', ' '))
 
     aux = deletar[1:-1]
    
+'''
+ fazendo uma sequência de condições para acessar os jogos
+  e a horas e adicionando ao dicionário
+'''
 
 for i in aux:
     info = str(i).split()
@@ -86,6 +94,55 @@ for i in aux:
             dados['horario'].append(info[3])
 
 # USANDO PANDAS
+df = pd.DataFrame(dados)
 
-df = pandas.DataFrame(dados)
-df.to_csv('jogos_do_dia')
+arquivo = df[['horario', 'times']].groupby(['horario', 'times']).max()
+arquivo.to_csv('jogos_do_dia')
+
+# ENVIANDO O EMAIL 
+
+# configurando variáveis e iniciando o servidor
+host = 'smtp.gmail.com'
+port = 587 
+user = 'remetente'
+password = 'senha'
+
+server = smtplib.SMTP(host=host, port=port)
+
+server.ehlo()
+server.starttls()
+server.login(user, password)
+
+# partes do email 
+message = MIMEMultipart()
+message['From'] = user
+message['To'] = 'destinatario'
+message['Subject'] = 'Jogos do dia'
+
+# fixando mensagem ao email
+message.attach(MIMEText('''
+    Olá! Segue a lista dos jogos do dia de hoje :)
+''', 'plain'))
+
+
+# parte do anexo
+filename = 'jogos_do_dia'
+attachment = open('jogos_do_dia', 'rb')
+
+att = MIMEBase('application', 'octet-stream')
+att.set_payload(attachment.read())
+encoders.encode_base64(att)
+
+att.add_header('Content-Disposition', f'attachment; filename= {filename}')
+attachment.close()
+message.attach(att)
+
+# enviando o emil
+try:
+    server.sendmail(message['From'], message['To'], message.as_string())
+    print('Email enviado com sucesso!')
+except Exception as e:
+    print(e)
+
+# fechando o servidor
+server.quit()
